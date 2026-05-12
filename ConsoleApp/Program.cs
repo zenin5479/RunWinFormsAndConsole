ca35ab3f3049b8d98ddb6337a261c02923b21d05
zenin5479 <zenin5479@gmail.com>
2026-05-12 21:59:58 +0300

    Update 0.6 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,17 +8,17 @@ namespace ConsoleApp
    {
       static void Main()
       {
-         string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "console_debug.log");
+         //string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "console_debug.log");
 
-         // Пишем всё, что происходит, в файл на рабочем столе
-         File.AppendAllText(logPath, string.Format("[{0}] Запуск консоли...\n", DateTime.Now));
+         //// Пишем всё, что происходит, в файл на рабочем столе
+         //File.AppendAllText(logPath, string.Format("[{0}] Запуск консоли...\n", DateTime.Now));
 
          try
          {
             // Гарантированно держим окно открытым несколько секунд
             Console.Title = "ДИАГНОСТИКА – НЕ ЗАКРЫВАТЬ";
             Console.WriteLine("Консоль запущена и будет ждать 5 секунд.");
-            File.AppendAllText(logPath, "Консоль запущена успешно.\n");
+            //File.AppendAllText(logPath, "Консоль запущена успешно.\n");
 
             // Даже если дальше будет исключение, окно останется открытым из-за задержки
             Thread.Sleep(5000); // 5 секунд
@@ -26,11 +26,11 @@ namespace ConsoleApp
             // Только после этого ждём нажатия клавиши
             Console.WriteLine("Нажмите любую клавишу для выхода...");
             Console.ReadKey();
-            File.AppendAllText(logPath, "Программа завершена нормально.\n");
+            //File.AppendAllText(logPath, "Программа завершена нормально.\n");
          }
          catch (Exception ex)
          {
-            File.AppendAllText(logPath, string.Format("ОШИБКА: {0}\n", ex));
+            //File.AppendAllText(logPath, string.Format("ОШИБКА: {0}\n", ex));
             Console.WriteLine("Произошла ошибка! Подробности в файле на рабочем столе.");
             Console.ReadKey(); // окно не закроется
          }
