353f7c56f2c5cdf63fe174606e171af93b8e73d4
zenin5479 <zenin5479@gmail.com>
2026-05-13 20:04:33 +0300

    Update 2.7 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.Designer.cs
+++ WinFormsApp/MainForm.Designer.cs
@@ -46,7 +46,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(412, 325);
+         ClientSize = new System.Drawing.Size(239, 108);
          Controls.Add(button1);
          MaximizeBox = false;
          MinimizeBox = false;
