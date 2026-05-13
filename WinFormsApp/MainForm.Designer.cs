d0d20184a12ffd98a39cb4d684cc5b7f83f0b287
zenin5479 <zenin5479@gmail.com>
2026-05-13 20:08:17 +0300

    Update 3.3 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.Designer.cs
+++ WinFormsApp/MainForm.Designer.cs
@@ -36,7 +36,7 @@
          button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
          button1.Location = new System.Drawing.Point(12, 12);
          button1.Name = "button1";
-         button1.Size = new System.Drawing.Size(210, 47);
+         button1.Size = new System.Drawing.Size(210, 37);
          button1.TabIndex = 0;
          button1.Text = "Запустить Console";
          button1.UseVisualStyleBackColor = true;
@@ -46,7 +46,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(234, 81);
+         ClientSize = new System.Drawing.Size(234, 61);
          Controls.Add(button1);
          MaximizeBox = false;
          MinimizeBox = false;
