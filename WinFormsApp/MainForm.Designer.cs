f7225b656485c541180f251225d4eed7d4161c22
zenin5479 <zenin5479@gmail.com>
2026-05-13 20:07:24 +0300

    Update 3.2 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.Designer.cs
+++ WinFormsApp/MainForm.Designer.cs
@@ -33,10 +33,10 @@
          // 
          // button1
          // 
-         button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+         button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
          button1.Location = new System.Drawing.Point(12, 12);
          button1.Name = "button1";
-         button1.Size = new System.Drawing.Size(210, 57);
+         button1.Size = new System.Drawing.Size(210, 47);
          button1.TabIndex = 0;
          button1.Text = "Запустить Console";
          button1.UseVisualStyleBackColor = true;
