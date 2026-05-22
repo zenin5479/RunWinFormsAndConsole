121941bb24d66f7ef1bcbb659a4505bbe441ef3e
zenin5479 <zenin5479@gmail.com>
2026-05-22 09:47:13 +0300

    Update 4.3 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp
             try
             {
                // Гарантированно держим окно открытым несколько секунд
-               Console.Title = "ДИАГНОСТИКА – НЕ ЗАКРЫВАТЬ";
+               Console.Title = "Диагностика – не ЗАКРЫВАТЬ";
                Console.WriteLine("Консоль запущена и будет ждать 5 секунд");
                File.AppendAllText(logPath, "Консоль запущена успешно\n");
 
